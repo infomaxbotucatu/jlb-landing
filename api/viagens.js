f274c5b6dd36ce313ecb0cd3fa61a4ec07c3a2d4
Marcos Antonio Gonçalves <infomaxbotucatu@gmail.com>
2026-02-17 15:12:16 -0300

    Integrando planilha Google Sheets

--- api/viagens.js
+++ api/viagens.js
@@ -1,6 +1,8 @@
 export default async function handler(req, res) {
   const sheetId = "1IEcz1DiCJlTOrx1PCpUYFf9DzugD2YI6z0g4U50ERK0";
-  const url = `https://opensheet.elk.sh/${sheetId}/viagens`;
+  const sheetName = "Viagens";
+
+  const url = `https://opensheet.elk.sh/${sheetId}/${sheetName}`;
 
   try {
     const response = await fetch(url);
@@ -8,6 +10,9 @@ export default async function handler(req, res) {
 
     res.status(200).json(data);
   } catch (error) {
-    res.status(500).json({ erro: "Erro ao buscar planilha" });
+    res.status(500).json({
+      erro: "Erro ao buscar dados da planilha",
+      detalhe: error.message
+    });
   }
 }
